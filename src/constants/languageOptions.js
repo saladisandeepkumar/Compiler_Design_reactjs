41115fb282e9f84ee63f832326e6ab5566d6b75d
sandeep kumar saladi <119839533+saladisandeepkumar@users.noreply.github.com>
2025-02-16 09:28:18 +0530

    Add files via upload

---
+++ src/constants/languageOptions.js
@@ -0,0 +1,103 @@
+export const languageOptions = [
+    {
+        id: 63,
+        name: "JavaScript (Node.js 12.14.0)",
+        label: "JavaScript (Node.js 12.14.0)",
+        value: "javascript",
+    },
+    
+    {
+        id: 75,
+        name: "C (Clang 7.0.1)",
+        label: "C (Clang 7.0.1)",
+        value: "c",
+    },
+    {
+        id: 76,
+        name: "C++ (Clang 7.0.1)",
+        label: "C++ (Clang 7.0.1)",
+        value: "cpp",
+    },
+    {
+        id: 48,
+        name: "C (GCC 7.4.0)",
+        label: "C (GCC 7.4.0)",
+        value: "c",
+    },
+    {
+        id: 52,
+        name: "C++ (GCC 7.4.0)",
+        label: "C++ (GCC 7.4.0)",
+        value: "cpp",
+    },
+    {
+        id: 49,
+        name: "C (GCC 8.3.0)",
+        label: "C (GCC 8.3.0)",
+        value: "c",
+    },
+    {
+        id: 53,
+        name: "C++ (GCC 8.3.0)",
+        label: "C++ (GCC 8.3.0)",
+        value: "cpp",
+    },
+    {
+        id: 50,
+        name: "C (GCC 9.2.0)",
+        label: "C (GCC 9.2.0)",
+        value: "c",
+    },
+    {
+        id: 54,
+        name: "C++ (GCC 9.2.0)",
+        label: "C++ (GCC 9.2.0)",
+        value: "cpp",
+    },
+
+    {
+        id: 51,
+        name: "C# (Mono 6.6.0.161)",
+        label: "C# (Mono 6.6.0.161)",
+        value: "csharp",
+    },
+   
+
+    {
+        id: 62,
+        name: "Java (OpenJDK 13.0.1)",
+        label: "Java (OpenJDK 13.0.1)",
+        value: "java",
+    },
+    {
+        id: 68,
+        name: "PHP (7.4.1)",
+        label: "PHP (7.4.1)",
+        value: "php",
+    },
+
+    {
+        id: 70,
+        name: "Python (2.7.17)",
+        label: "Python (2.7.17)",
+        value: "python",
+    },
+    {
+        id: 71,
+        name: "Python (3.8.1)",
+        label: "Python (3.8.1)",
+        value: "python",
+    },
+    {
+        id: 80,
+        name: "R (4.0.0)",
+        label: "R (4.0.0)",
+        value: "r",
+    },
+    {
+        id: 82,
+        name: "SQL (SQLite 3.27.2)",
+        label: "SQL (SQLite 3.27.2)",
+        value: "sql",
+    },
+];
